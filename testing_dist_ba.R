38c4cec59e7b218f6f19c2740f6b3b23f81ba103
Kate <katherine.hayes@ucdenver.edu>
2020-08-21 17:20:30 -0600

    Changed Dalton plot 15_3 from quadrant 0.1 to 0.2. Cleaned up models of basal area

--- testing_dist_ba.R
+++ testing_dist_ba.R
@@ -9,52 +9,63 @@ library(cowplot); theme_set(theme_cowplot())
 # options(scipen = 9999)
 
 # bringing in data
-ba <- read.csv(("ba.csv"), stringsAsFactors = F)
+ba <- read.csv(here("ba.csv"), stringsAsFactors = F)
 
   # removing unburned sites
   ba <- ba[ba$TREAT != 0,]
   
   # looking at format of data, first three rows
   ba[1:3,]
+  ba$BA_ha <- ba$BA * 50
 
 #####################################################   
 ## conifer basal area
 ##
-    
-    CONIF_BA <- subset(ba, ba$DIV == "c")	
+CONIF_BA <- ba %>%
+    filter(DIV == "c") %>%
+    group_by(SITE, TREAT, PLOT) %>%
+    summarise(BA = sum(BA_ha)) %>%
+    ungroup() %>%
+    complete(SITE, TREAT, PLOT, fill = list(BA = 0))
+ hist(CONIF_BA$BA)
+ CONIF_BA[CONIF_BA$BA > 100,] # checking high value
   
-    mC_BA <- lm(log(BA) ~ TREAT + SITE + (TREAT*SITE), data = CONIF_BA) 
+    mC_BA.lm <- lm(BA ~ TREAT + SITE + (TREAT*SITE), data = CONIF_BA) 
+    mC_BA.log <- lm(log(BA + 1) ~ TREAT + SITE + (TREAT*SITE), data = CONIF_BA) 
+    mC_BA.ga <- glm(BA ~ TREAT + SITE + (TREAT*SITE), family = gaussian(link = "identity"), data = CONIF_BA)
+    
     # residuals
-    plot(mC_BA)
+    par(mfrow=c(3,4))
+    plot(mC_BA.lm, main = "Linear")
+    plot(mC_BA.log, main = "logged")
+    plot(mC_BA.ga, main = "Gaussian")
+    
     # parameters
-    summary(mC_BA); #model_parameters(mC_BA)
-    # plots
-    # plot_model(mC_BA, show.values = T, show.p = T, sort.est = T) # effect sizes
-    # plot_model(mC_BA, type = "pred", terms = c("TREAT", "SITE")) # predicted values
+    summary(mC_BA.lm); #model_parameters(mC_BA)
+    summary(mC_BA.log)
+    summary(mC_BA.ga)
     
 #################################################    
 ## deciduous basal area
 ##     
-
-    #Different way of subsetting, base R  
-    DECID_BA <- subset(ba, ba$DIV == "d")
-    
-    #the plots are duplicated with one entry per conifer species, so this reduces it to one value per plot
-    cnt <- aggregate(DECID_BA$BA, list(DECID_BA$PLOT), FUN = sum)  	#adds up the count per ha for each row within a plot
-    trt <- aggregate(DECID_BA$TREAT, list(DECID_BA$PLOT), FUN = mean)	#gets the treatment number (mean works because they are the same
-    DECID_BA_new <- cbind(cnt, trt[,2])						#combines
-    
-    temp <- merge(x=DECID_BA_new, y=DECID_BA, by.x="Group.1", by.y="PLOT", all.x=F, all.y=F)	#adding in the site in a clumsy way but it was quick
-    DECID_BA_new <- temp[duplicated(temp[,1]),]					#merging and removing
-    DECID_BA_new <- DECID_BA_new[,1:4]						#remove extra columns
-    
-    names(DECID_BA_new) <- c("PLOT","BA","TREAT", "SITE")			#get names back in there
-    DECID_BA <- DECID_BA_new								#back to original name so code below works
-    hist(DECID_BA$BA)			
+    DECID_BA <- ba %>%
+      filter(DIV == "d") %>%
+      group_by(SITE, TREAT, PLOT) %>%
+      summarise(BA = sum(BA_ha)) %>%
+      ungroup() %>%
+      complete(SITE, TREAT, PLOT, fill = list(BA = 0))
+    hist(DECID_BA$BA)
   
-    mD_BA <- lm(log(BA) ~ TREAT + SITE + (TREAT*SITE), data = DECID_BA) 
-  # residuals
-  plot(mD_BA)
+  mD_BA.lm <- lm(BA  ~ TREAT + SITE + (TREAT*SITE), data = DECID_BA)
+  mD_BA.log <- lm(log(BA+1)  ~ TREAT + SITE + (TREAT*SITE), data = DECID_BA)
+  mD_BA.ga <- glm(BA  ~ TREAT + SITE + (TREAT*SITE), family = gaussian(link = "identity"), data = DECID_BA)
+  
+  par(mfrow=c(3,4))
+  plot(mD_BA.lm, main="Linear")
+  plot(mD_BA.log, main= "Logged")
+  plot(mD_BA.ga, main="Guassian")
+  
   # parameters
-  summary(mD_BA); # model_parameters(mD_BA)
-  
+  summary(mD_BA.lm); # model_parameters(mD_BA)
+  summary(mD_BA.log)
+  summary(mD_BA.ga)
