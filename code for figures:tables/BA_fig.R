fe0b19bbd866aa585c0bdfdcb379c9984ec860a5
Kate <katherine.hayes@ucdenver.edu>
2020-08-26 14:01:47 -0600

    update basal area figure

--- code for figures:tables/BA_fig.R
+++ code for figures:tables/BA_fig.R
@@ -6,15 +6,15 @@ library(here)
 theme_set(theme_cowplot())
 options(scipen = 9999)
 
-species_ba <- read.csv(here("species_ba.csv"), stringsAsFactors = F)
+ba <- read.csv(here("ba.csv"), stringsAsFactors = F)
 
-species_ba$SITE[species_ba$SITE == "DALTON"] <- "Upland"
-species_ba$SITE[species_ba$SITE == "STEESE"] <- "Lowland"
+ba$SITE[ba$SITE == "DALTON"] <- "Upland"
+ba$SITE[ba$SITE == "STEESE"] <- "Lowland"
 
-species_ba %>%
+ba %>%
   filter(TREAT != 0) %>%
-  ggplot(aes(x = as.factor(TREAT), y = BA_PLOT, fill = DIV)) + 
-  geom_boxplot() + facet_wrap(~SITE) + ylim(0,6) +
+  ggplot(aes(x = as.factor(TREAT), y = BA_ha, fill = DIV)) + 
+  geom_boxplot() + facet_wrap(~SITE) + ylim(0,0.35) + 
   labs(x = "Number of Fires", y = "Basal Area (m2/ha)", 
        title = "Total Basal Area in Burned Plots") + 
   scale_fill_manual(values = c("#f0f0f0", "#bdbdbd"),
